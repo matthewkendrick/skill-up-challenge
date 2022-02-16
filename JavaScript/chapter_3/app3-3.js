4088d7474f895eb5457b60234f5058d4ee1697a2
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-02-17 06:30:57 +0900

    [Add]: app3-3.js
    Message body
    Defining multiple functions
    
    Message footer
    JavaScript_skill-up-challenge

---
+++ JavaScript/chapter_3/app3-3.js
@@ -0,0 +1,55 @@
+// 複数の関数を定義する
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+function getJShand() {
+  let js_hand_num = Math.floor( Math.random() * 3);
+  let hand_name;
+
+  if (js_hand_num == 0) {
+    hand_name = "グー";
+  } else if (js_hand_num == 1){
+    hand_name = "チョキ"
+  } else if (js_hand_num == 2){
+    hand_name = "パー"
+  }
+  
+  return hand_name;
+}
+
+function winLose(user, js) {
+  let winLoseStr;
+
+  if (user == "グー") {
+    if (js == "グー") {
+      winLoseStr = "あいこ"
+    } else if (js == "チョキ"){
+      winLose = "勝ち"
+    } else if (js == "パー"){
+      winLose = "負け"
+    }
+  } else if (user == "チョキ"){
+    if (js == "グー") {
+      winLoseStr = "負け"
+    } else if (js == "チョキ"){
+      winLoseStr = "あいこ"
+    } else if (js == "パー"){
+      winLoseStr = "勝ち"
+    }
+  } else if (user == "パー"){
+    if (js == "グー") {
+      winLoseStr = "勝ち"
+    } else if (js == "チョキ"){
+      winLoseStr = "負け"
+    } else if (js == "パー"){
+      winLoseStr = "あいこ"
+    }
+  }
+
+  return winLoseStr;
+}
