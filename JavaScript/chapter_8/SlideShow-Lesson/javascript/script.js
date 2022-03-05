5c308ac84d728976edbaea05ff08055350a7bfcc
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-03-05 20:44:32 +0900

    [Edit]: SlideShow-Lesson
    Message body
    Finish the lesson for SlideShow
    
    Message footer
    JavaScript_skill-up-challenge

---
+++ JavaScript/chapter_8/SlideShow-Lesson/javascript/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition: 'fade',
+  speed: 1000,
+  easing: 'easeOutQuart',
+  navType: 'bubble',
+  childrenElementType: 'div',
+  arrows: false,
+  autoPlay: true,
+  autoPlayDuration: 10000,
+  keyboardOnAlways: true,
+  hidePrevious: false,
+});
